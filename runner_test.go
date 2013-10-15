d91f3f148ded3fdad7b70a3e649320f9003f655b
Jeremy Saenz <jeremy.saenz@gmail.com>
2013-10-15 06:43:03 -0700

    Started tests for the Runner type

---
+++ runner_test.go
@@ -0,0 +1,26 @@
+package gin_test
+
+import (
+	"github.com/codegangsta/gin"
+	"os/exec"
+	"testing"
+)
+
+func Test_NewRunner(t *testing.T) {
+	bin := "test_fixtures/build_success/build_success"
+	runner := gin.NewRunner(bin)
+
+	expect(t, runner.BinInfo().Name(), "build_success")
+}
+
+func Test_Runner_Run(t *testing.T) {
+	bin := "test_fixtures/build_success/build_success"
+	runner := gin.NewRunner(bin)
+
+	command, err := runner.Run()
+}
+
+// func Test_Runner_SettingEnvironment
+// func Test_Runner_WritingOutput
+// func Test_Runner_ThrowingError
+// func Test_Runner_RestartingUpdatedBinary
