5a1d4935b1c1a808ad42e967104b1301614d64e4
Jeremy Saenz <jeremy.saenz@gmail.com>
2013-10-17 17:59:28 -0700

    Running one test

--- runner_test.go
+++ runner_test.go
@@ -12,12 +12,12 @@ func Test_NewRunner(t *testing.T) {
 	expect(t, runner.BinInfo().Name(), "build_success")
 }
 
-func Test_Runner_Run(t *testing.T) {
-	bin := "test_fixtures/build_success/build_success"
-	runner := gin.NewRunner(bin)
-
-	command, err := runner.Run()
-}
+// func Test_Runner_Run(t *testing.T) {
+// 	bin := "test_fixtures/build_success/build_success"
+// 	runner := gin.NewRunner(bin)
+// 
+// 	command, err := runner.Run()
+// }
 
 // func Test_Runner_SettingEnvironment
 // func Test_Runner_WritingOutput
