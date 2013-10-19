dbe3ee4551b5d8943ea1584d87a07fe106413b72
Jeremy Saenz <jeremy.saenz@gmail.com>
2013-10-19 13:55:15 -0700

    More runner tests

--- runner.go
+++ runner.go
@@ -1,1 +1,27 @@
 package gin
+
+import (
+	"os"
+	"os/exec"
+)
+
+type Runner interface {
+	Run() (*exec.Cmd, error)
+	Info() (os.FileInfo, error)
+}
+
+type runner struct {
+	bin string
+}
+
+func NewRunner(bin string) Runner {
+	return &runner{bin: bin}
+}
+
+func (r *runner) Run() (*exec.Cmd, error) {
+	return nil, nil
+}
+
+func (r *runner) Info() (os.FileInfo, error) {
+	return os.Stat(r.bin)
+}
--- runner_test.go
+++ runner_test.go
@@ -9,17 +9,22 @@ func Test_NewRunner(t *testing.T) {
 	bin := "test_fixtures/build_success/build_success"
 	runner := gin.NewRunner(bin)
 
-	expect(t, runner.BinInfo().Name(), "build_success")
+	fi, _ := runner.Info()
+	expect(t, fi.Name(), "build_success")
 }
 
-// func Test_Runner_Run(t *testing.T) {
-// 	bin := "test_fixtures/build_success/build_success"
-// 	runner := gin.NewRunner(bin)
-// 
-// 	command, err := runner.Run()
+func Test_Runner_Run(t *testing.T) {
+	bin := "test_fixtures/build_success/build_success"
+	runner := gin.NewRunner(bin)
+
+	cmd, err := runner.Run()
+	expect(t, err, nil)
+	expect(t, cmd.Dir, "foobar")
+}
+
+// func Test_Runner_SettingEnvironment(t *testing.T) {
 // }
 
-// func Test_Runner_SettingEnvironment
 // func Test_Runner_WritingOutput
 // func Test_Runner_ThrowingError
 // func Test_Runner_RestartingUpdatedBinary
