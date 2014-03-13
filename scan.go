f8484cd22254decfe0ccac7d3e1526379a05726c
Harley Laue <losinggeneration@gmail.com>
2014-03-13 15:16:24 -0500

    Move the scanChanges function into it's own file

---
+++ scan.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func scanChanges(watchPath string, cb scanCallback) {
+	for {
+		filepath.Walk(watchPath, func(path string, info os.FileInfo, err error) error {
+			if path == ".git" {
+				return filepath.SkipDir
+			}
+
+			if filepath.Ext(path) == ".go" && info.ModTime().After(startTime) {
+				cb(path)
+				startTime = time.Now()
+				return errors.New("done")
+			}
+
+			return nil
+		})
+		time.Sleep(500 * time.Millisecond)
+	}
+}
