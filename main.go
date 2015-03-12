3068cfd20c9f5774fbceacecf58c50400cdc5b24
Oscar Swanros <swanros@gmail.com>
2015-03-12 15:19:59 -0600

    restart the server if a tmpl file is changed, too

--- main.go
+++ main.go
@@ -169,7 +169,7 @@ func scanChanges(watchPath string, cb scanCallback) {
 				return nil
 			}
 
-			if filepath.Ext(path) == ".go" && info.ModTime().After(startTime) {
+			if (filepath.Ext(path) == ".go" || filepath.Ext(path) == ".tmpl") && info.ModTime().After(startTime) {
 				cb(path)
 				startTime = time.Now()
 				return errors.New("done")
